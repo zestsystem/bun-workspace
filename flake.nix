1c5242b0f8fb82b8947d93b76a23191a646266db
zestsystem <mikeyim@unitetheculture.org>
2023-08-12 10:36:41 -0700

    init project

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Bun Workspace";
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      formatter = pkgs.alejandra;
+
+      devShells = {
+        default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            bun
+            git-lfs
+            zsh
+          ];
+        };
+      };
+    });
+}
